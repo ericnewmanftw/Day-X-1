51032e4e63169a58c4f288ae2920201d968b460b
Alan Barth <alancbarth@gmail.com>
2015-04-07 20:21:55 -0600

    Initial Commit

---
+++ DayX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayX
+//
+//  Created by Alan Barth on 4/7/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DayX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayX
+//
+//  Created by Alan Barth on 4/7/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
